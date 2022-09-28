e6218e5595102e8ade5e147902d78ba8cac86d8b
Marcin-Matczak <marcorn.88@gmail.com>
2022-09-28 20:19:18 +0200

    Added Search component to filter cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styles from "./App.module.scss";
 import PropTypes from "prop-types";
 import List from "./components/List/ListContainer.js";
+import Search from "./components/Search/SearchContainer";
 
 function App(props) {
   const { title, subtitle, lists } = props;
@@ -8,6 +9,7 @@ function App(props) {
     <main className={styles.component}>
       <h1 className={styles.title}>{title}</h1>
       <h2 className={styles.subtitle}>{subtitle}</h2>
+      <Search />
       {lists.map(listData => (<List key={listData.id} {...listData} />))}
     </main>
   );
